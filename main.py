305271b7e8a0a0f4f489bceaa36688fc73d39846
Kalia7 <kaliaevelin7@gmail.com>
2026-01-12 21:52:37 -0300

    Código basico para Radar de Velocidade

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Teste de commit")
+velocidade = 0
+
+input_velocidade = int(input("Digite a velocidade do carro em km/h: "))
+if input_velocidade > 80:
+    velocidade = input_velocidade - 80
+    multa = velocidade * 7
+    print(f"Você foi multado! Sua multa é de R$ {multa:.2f}")
+else:
+    print("Tenha um bom dia! Dirija com segurança!")
